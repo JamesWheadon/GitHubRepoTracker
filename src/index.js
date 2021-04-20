5b5ffc6e8b221af21b8701d5410b64209deeb3bb
Ravil Shafiyev <ravil-96@mail.ru>
2021-04-20 16:53:14 +0100

    fixing provider wrapper bug

--- src/index.js
+++ src/index.js
@@ -2,17 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import store from './store';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider.StrictMode>
+    <Provider store={store}>
       <Router>
         <App />
       </Router>
-    </Provider.StrictMode>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
