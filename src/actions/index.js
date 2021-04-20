a621e2ff208dabf1c3a3186153cd7ecd59744af6
James Wheadon <james.wheadon@yahoo.com>
2021-04-20 15:17:11 +0100

    map data from API call to repoData with required data

--- src/actions/index.js
+++ src/actions/index.js
@@ -6,6 +6,10 @@ export const getUserData = (username) => {
         if (data.status === 404) {
             throw Error('Invalid Username');
         }
+        const repoData = data.map(d => {
+            return {name: d.name, url: d.html_url, issues: d.open_issues_count, forks: d.forks, stargazers: d.stargazers_count, language: d.language, lastUpdated: d.updated_at};
+        });
+        return repoData;
     } catch (err) {
         throw new Error(err.message);
     }
