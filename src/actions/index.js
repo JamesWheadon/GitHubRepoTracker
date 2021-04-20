7a72d5bd3d9ca77bf2108c3040c9ec05e460fae9
James Wheadon <james.wheadon@yahoo.com>
2021-04-20 15:22:25 +0100

    refactor getUserData for redux dispatch

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,16 +1,20 @@
 import axios from 'axios';
 
-export const getUserData = (username) => {
-    try {
-        const data = await axios.get(`https://api.github.com/users/${username}`);
-        if (data.status === 404) {
-            throw Error('Invalid Username');
+export const getUserData = username => {
+    return async dispatch => {
+        try {
+            const data = await fetchRepoData(username)
+            //axios.get(`https://api.github.com/users/${username}`);
+            //if (data.status === 404) {
+            //    throw Error('Invalid Username');
+            //}
+            const repoData = data.map(d => {
+                return {name: d.name, url: d.html_url, issues: d.open_issues_count, forks: d.forks, stargazers: d.stargazers_count, language: d.language, lastUpdated: d.updated_at};
+            });
+            dispatch(loadResult(repoData));
+        } catch (err) {
+            console.warn(err.message);
+            dispatch({ type: 'SET_ERROR', payload: err.message })
         }
-        const repoData = data.map(d => {
-            return {name: d.name, url: d.html_url, issues: d.open_issues_count, forks: d.forks, stargazers: d.stargazers_count, language: d.language, lastUpdated: d.updated_at};
-        });
-        return repoData;
-    } catch (err) {
-        throw new Error(err.message);
     }
 }
