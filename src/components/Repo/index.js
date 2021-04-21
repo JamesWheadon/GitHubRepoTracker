ac1c2a7f4ab4dc68c3e08c4461af3a3747cd57a3
James Wheadon <james.wheadon@yahoo.com>
2021-04-21 10:12:57 +0100

    logic for repos older than 1 month

--- src/components/Repo/index.js
+++ src/components/Repo/index.js
@@ -1,13 +1,30 @@
 import React from 'react';
 
 function Repo ({ repoData }) {
+
+    const getDate = () => {
+        const currentTime = new Date();
+        const updateTime = new Date(repoData.lastUpdated);
+        const timeDiff = (currentTime - updateTime) / 1000;
+        let lastUpdated;
+        if (currentTime.getFullYear() != updateTime.getFullYear()) {
+            lastUpdated = `${updateTime.toLocaleString('default', { month: 'short' })} ${updateTime.getDate()} ${updateTime.getFullYear()}`;
+            return lastUpdated;
+        }
+        if (timeDiff > 86400) {
+            lastUpdated = `${updateTime.toLocaleString('default', { month: 'short' })} ${updateTime.getDate()}`;
+            return lastUpdated;
+        }
+        return 'test';
+    }
+
     return (
         <>
             <div className="repo">
                 <div className="repoTitle">
                 <h3><a href={repoData.url}>{repoData.name}</a></h3>
                 <h4>Language: {repoData.language}</h4>
-                <h4>Last updated: {repoData.lastUpdated}</h4>
+                <h4>Updated on {getDate()}</h4>
                 </div>
                 <ul className="repoData">
                     <li>Issues: {repoData.issues}</li>
