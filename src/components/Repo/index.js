96c080c820430703fd34f533b860e4a00e2f0e80
James Wheadon <james.wheadon@yahoo.com>
2021-04-21 09:21:54 +0100

    basic styling for repositories

--- src/components/Repo/index.js
+++ src/components/Repo/index.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
 function Repo ({ repoData }) {
-    console.log(repoData)
     return (
-        <div>
-            <h3>{repoData.name}</h3>
-            <h4>Last updated: {repoData.lastUpdated}</h4>
+        <div className="repo">
+            <div className="repoTitle">
+            <h3><a href={repoData.url}>{repoData.name}</a></h3>
             <h4>Language: {repoData.language}</h4>
-            <ul>
+            <h4>Last updated: {repoData.lastUpdated}</h4>
+            </div>
+            <ul className="repoData">
                 <li>Issues: {repoData.issues}</li>
                 <li>Stargazers: {repoData.stargazers}</li>
                 <li>Forks: {repoData.forks}</li>
             </ul>
-            <a href={repoData.url}>See more</a>
         </div>
     );
 };
