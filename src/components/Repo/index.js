e8f3839b319251c878804e6dcbd80585f1fa94d5
James Wheadon <james.wheadon@yahoo.com>
2021-04-21 09:32:03 +0100

    further styling

--- src/components/Repo/index.js
+++ src/components/Repo/index.js
@@ -2,18 +2,21 @@ import React from 'react';
 
 function Repo ({ repoData }) {
     return (
-        <div className="repo">
-            <div className="repoTitle">
-            <h3><a href={repoData.url}>{repoData.name}</a></h3>
-            <h4>Language: {repoData.language}</h4>
-            <h4>Last updated: {repoData.lastUpdated}</h4>
+        <>
+            <div className="repo">
+                <div className="repoTitle">
+                <h3><a href={repoData.url}>{repoData.name}</a></h3>
+                <h4>Language: {repoData.language}</h4>
+                <h4>Last updated: {repoData.lastUpdated}</h4>
+                </div>
+                <ul className="repoData">
+                    <li>Issues: {repoData.issues}</li>
+                    <li>Stargazers: {repoData.stargazers}</li>
+                    <li>Forks: {repoData.forks}</li>
+                </ul>
             </div>
-            <ul className="repoData">
-                <li>Issues: {repoData.issues}</li>
-                <li>Stargazers: {repoData.stargazers}</li>
-                <li>Forks: {repoData.forks}</li>
-            </ul>
-        </div>
+            <hr />
+        </>
     );
 };
 
