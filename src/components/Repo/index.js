dd7e00f28e72c81bb8437e32304b09c75cdc1494
James Wheadon <james.wheadon@yahoo.com>
2021-04-21 11:12:08 +0100

    access data for each language colour

--- src/components/Repo/index.js
+++ src/components/Repo/index.js
@@ -1,6 +1,14 @@
 import React from 'react';
+import data from './languageColours.json';
 
-function Repo ({ repoData }) {
+function Repo({ repoData }) {
+
+    const getColours = (language) => {
+        if (language) {
+            console.log(data[language].color)
+            return data[language].color;
+        }
+    }
 
     const getDate = () => {
         const currentTime = new Date();
@@ -35,9 +43,9 @@ function Repo ({ repoData }) {
         <>
             <div className="repo">
                 <div className="repoTitle">
-                <h3><a href={repoData.url}>{repoData.name}</a></h3>
-                <h4>Language: {repoData.language}</h4>
-                <h4>Updated {getDate()}</h4>
+                    <h3><a href={repoData.url}>{repoData.name}</a></h3>
+                    <h4>{repoData.language}</h4>
+                    <h4>Updated {getDate()}</h4>
                 </div>
                 <ul className="repoData">
                     <li><svg aria-label="fork" height="16" viewBox="0 0 16 16" version="1.1" width="16" aria-hidden="true"><path fillRule="evenodd" d="M5 3.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm0 2.122a2.25 2.25 0 10-1.5 0v.878A2.25 2.25 0 005.75 8.5h1.5v2.128a2.251 2.251 0 101.5 0V8.5h1.5a2.25 2.25 0 002.25-2.25v-.878a2.25 2.25 0 10-1.5 0v.878a.75.75 0 01-.75.75h-4.5A.75.75 0 015 6.25v-.878zm3.75 7.378a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm3-8.75a.75.75 0 100-1.5.75.75 0 000 1.5z"></path></svg>{repoData.forks}</li>
